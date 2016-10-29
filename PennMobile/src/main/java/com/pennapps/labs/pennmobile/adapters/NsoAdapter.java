fbc67e4df635514cf3bc6482eba92c973905486f
Andrew Si <andrewsi10@gmail.com>
2016-10-28 22:50:13 -0400

    in-progress NSO calendar fix; resume in spring

--- PennMobile/src/main/java/com/pennapps/labs/pennmobile/adapters/NsoAdapter.java
+++ PennMobile/src/main/java/com/pennapps/labs/pennmobile/adapters/NsoAdapter.java
@@ -1,8 +1,15 @@
 package com.pennapps.labs.pennmobile.adapters;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.preference.PreferenceManager;
+import android.provider.CalendarContract;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +31,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
+import java.util.TimeZone;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -62,7 +70,53 @@ public class NsoAdapter extends ArrayAdapter<RSSItem> {
             @Override
             public void onClick(View view) {
                 //to be implemented
+                // add event to user's calendar
+                ContentResolver cr = mContext.getContentResolver();
+                ContentValues values = new ContentValues();
 
+                try {
+                    values.put(CalendarContract.Events.DTSTART, getEventTime(item));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                values.put(CalendarContract.Events.TITLE, getTitleName(item));
+                values.put(CalendarContract.Events.DESCRIPTION, getDescription(item));
+
+                String time = item.getTitle();
+                time = time.substring(time.indexOf("event/") + 6);
+                time = time.substring(0, time.indexOf("/"));
+                // start/end times are correct but displaying the times have bugs
+                // e.g. 12:00 PM is displayed as 0:00 PM in the app
+                Log.d("starttime", time.substring(0, 17));
+                Log.d("endtime", time.substring(18));
+                String starttime = time.substring(0, 17);
+                String endtime = time.substring(18);
+
+                values.put(CalendarContract.Events.DTSTART, time.substring(0, 17));
+                values.put(CalendarContract.Events.DTEND, time.substring(18));
+
+                TimeZone timeZone = TimeZone.getDefault();
+                values.put(CalendarContract.Events.EVENT_TIMEZONE, timeZone.getID());
+
+                // default calendar
+                values.put(CalendarContract.Events.CALENDAR_ID, 1);
+
+                try {
+                    Log.d("calendar event details", getTitleName(item) + " @ " +
+                            getEventTime(item) + " " + getDescription(item));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                // insert event to calendar
+                if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_CALENDAR)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                // seems to fail to reach this stage; manifest does have write_calendar
+                // permission though
+                Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+                Log.d("added event", values.toString());
             }
         });
 
